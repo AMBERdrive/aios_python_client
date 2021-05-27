fa8d035d2f89de606d19d58905abb5145be4a025
EMhacker <emhacker_alt@163.com>
2021-05-27 13:41:47 +0800

    Update encoder calibration delay

--- encoder_calibration.py
+++ encoder_calibration.py
@@ -33,7 +33,7 @@ def main():
 
         print('\n')
 
-        time.sleep(15)
+        time.sleep(13)
         for i in range(len(Server_IP_list)):
             aios.encoderIsReady(Server_IP_list[i], 1)
         print('\n')
